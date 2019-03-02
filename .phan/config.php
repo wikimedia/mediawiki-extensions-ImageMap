eadde54449f175dbd648c6a218b577d9c3df73df
zoranzoki21 <zorandori4444@gmail.com>
2019-03-02 22:09:54 +0100

    Upgrade ImageMap extension to use newer phan
    Bug: T216921
    Change-Id: I6bb5c8e855a191cae7bddbf8a5f23da5c033045f

--- .phan/config.php
+++ .phan/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$cfg = require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 // Phan gets confused because DOMNode::setAttribute doesn't
 // exist, it's DOMElement::setAttribute, and some functions
