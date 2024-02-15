760641e18068706bc6257747c42882d83a30399e
Gergő Tisza <gtisza@wikimedia.org>
2024-02-15 21:22:28 +0000

    Filter out null external link attributes
    Follows up If4880e4e0b3cd2056521d44baf261ed848511bcd.
    
    Bug: T357668
    Change-Id: Ifec733a923f193b72eaba9a1e604ad4e56c0aef2

--- includes/ImageMap.php
+++ includes/ImageMap.php
@@ -247,7 +247,10 @@ class ImageMap implements ParserFirstCallInitHook {
 			$attribs = [];
 			if ( $externLink ) {
 				// Get the 'target' and 'rel' attributes for external link.
-				$attribs = $parser->getExternalLinkAttribs( $title );
+				$attribs = array_filter(
+					$parser->getExternalLinkAttribs( $title ),
+					fn ( $attr ) => $attr !== null
+				);
 
 				$attribs['href'] = $title;
 				$attribs['class'] = 'plainlinks';
