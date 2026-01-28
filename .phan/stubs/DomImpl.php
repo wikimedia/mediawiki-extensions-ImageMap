f75ad90df6351da535fd4c1a21a51053a3b39f41
C. Scott Ananian <cscott@cscott.net>
2026-01-28 01:04:33 -0500

    Use Wikimedia\Parsoid\DOM\* aliases to improve compatibility with PHP 8.4+
    We also fix up the phan configuration, and be more care to use
    DOMCompat methods where needed.
    
    Change-Id: I4d2cc57ed92e2b115895f75ccb12cbf28c088be0
    (cherry picked from commit 478b737b430951a7dd1bcbc197731fa472e204a4)

--- .phan/stubs/DomImpl.php
+++ .phan/stubs/DomImpl.php
@@ -1,14 +1,21 @@
 <?php
-class_alias( "DOMAttr", "Wikimedia\\Parsoid\\DOM\\Attr" );
-class_alias( "DOMCharacterData", "Wikimedia\\Parsoid\\DOM\\CharacterData" );
-class_alias( "DOMComment", "Wikimedia\\Parsoid\\DOM\\Comment" );
-class_alias( "DOMDocument", "Wikimedia\\Parsoid\\DOM\\Document" );
-class_alias( "DOMDocumentFragment", "Wikimedia\\Parsoid\\DOM\\DocumentFragment" );
-class_alias( "DOMDocumentType", "Wikimedia\\Parsoid\\DOM\\DocumentType" );
-class_alias( "DOMDOMException", "Wikimedia\\Parsoid\\DOM\\DOMException" );
-class_alias( "DOMDOMParser", "Wikimedia\\Parsoid\\DOM\\DOMParser" );
-class_alias( "DOMElement", "Wikimedia\\Parsoid\\DOM\\Element" );
-class_alias( "DOMNode", "Wikimedia\\Parsoid\\DOM\\Node" );
-class_alias( "DOMNodeList", "Wikimedia\\Parsoid\\DOM\\NodeList" );
-class_alias( "DOMProcessingInstruction", "Wikimedia\\Parsoid\\DOM\\ProcessingInstruction" );
-class_alias( "DOMText", "Wikimedia\\Parsoid\\DOM\\Text" );
+# For the purpose of phan, we're always using IDLeDOM.  That avoids the
+# type variance/co-variance issues involved in teaching phan about our
+# subclasses, and also keeps us from accessing any private implementation
+# details of Dodo.
+#
+# This list should match the one in DomImpl.php in the root.
+
+class_alias( "Wikimedia\\IDLeDOM\\Attr", "Wikimedia\\Parsoid\\DOM\\Attr" );
+class_alias( "Wikimedia\\IDLeDOM\\CharacterData", "Wikimedia\\Parsoid\\DOM\\CharacterData" );
+class_alias( "Wikimedia\\IDLeDOM\\Comment", "Wikimedia\\Parsoid\\DOM\\Comment" );
+class_alias( "Wikimedia\\IDLeDOM\\Document", "Wikimedia\\Parsoid\\DOM\\Document" );
+class_alias( "Wikimedia\\IDLeDOM\\DocumentFragment", "Wikimedia\\Parsoid\\DOM\\DocumentFragment" );
+class_alias( "Wikimedia\\IDLeDOM\\DocumentType", "Wikimedia\\Parsoid\\DOM\\DocumentType" );
+class_alias( "Wikimedia\\IDLeDOM\\DOMException", "Wikimedia\\Parsoid\\DOM\\DOMException" );
+class_alias( "Wikimedia\\IDLeDOM\\DOMParser", "Wikimedia\\Parsoid\\DOM\\DOMParser" );
+class_alias( "Wikimedia\\IDLeDOM\\Element", "Wikimedia\\Parsoid\\DOM\\Element" );
+class_alias( "Wikimedia\\IDLeDOM\\Node", "Wikimedia\\Parsoid\\DOM\\Node" );
+class_alias( "Wikimedia\\IDLeDOM\\NodeList", "Wikimedia\\Parsoid\\DOM\\NodeList" );
+class_alias( "Wikimedia\\IDLeDOM\\ProcessingInstruction", "Wikimedia\\Parsoid\\DOM\\ProcessingInstruction" );
+class_alias( "Wikimedia\\IDLeDOM\\Text", "Wikimedia\\Parsoid\\DOM\\Text" );
